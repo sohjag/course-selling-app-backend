d9bd55d7a08c52996845a2e2113c4d03ea46c0f6
Sohan <sohan.j12@gmail.com>
2023-08-08 16:52:19 +0530

    imported path lib

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 // const https = require("https");
 // const fs = require("fs");
